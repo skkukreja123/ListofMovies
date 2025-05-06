9647019b3526957c00682074d319d3ddaa793da1
skkukreja123 <sahil.kukreja@benchmatrix.ca>
2025-05-06 06:35:45 -0700

    model_define

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_managment/di/locator.dart';
+import 'package:state_managment/view/screen/home_screen.dart';
 
 void main() {
   setupLocator(); // Initialize the service locator
@@ -10,6 +11,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider();
+    return MaterialApp(
+      title: 'State Management Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: homeScreen(), // Set the home screen to homeScreen widget
+    );
   }
 }
---
+++ lib/view/screen/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatelessWidget {
+  const homeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}

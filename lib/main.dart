dfadf03cc0a3a6f586985006bb0adb94533cf014
skkukreja123 <sahil.kukreja@benchmatrix.ca>
2025-05-06 09:40:05 -0700

    render the movie details

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,24 @@ import 'package:provider/provider.dart';
 import 'package:state_managment/di/locator.dart';
 import 'package:state_managment/view/screen/home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:state_managment/viewmodel/movie_view.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
-  setupLocator(); // Initialize the service locator
+  await dotenv.load(fileName: 'assets/.env');
+  setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'State Management Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => getIt<MovieViewModel>()..getNowPlayingMovies(),
+      child: MaterialApp(
+        title: 'Movie App',
+        home: HomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: homeScreen(), // Set the home screen to homeScreen widget
     );
   }
 }
--- lib/viewmodel/movie_view.dart
+++ lib/viewmodel/movie_view.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:state_managment/data/service/movie_service.dart';
+import '../model/movie.dart';
+import '../core/error/failure.dart';
 
+class MovieViewModel extends ChangeNotifier {
+  final MovieService movieService;
+  MovieViewModel(this.movieService);
+
+  List<Movie> _movies = [];
+  List<Movie> get movies => _movies;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  String? _error;
+  String? get error => _error;
+
+  Future<void> getNowPlayingMovies() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _movies = await movieService.getNowPlayingMovies();
+    } catch (e) {
+      if (e is Failure) {
+        _error = e.message;
+      } else {
+        _error = 'Unexpected error: $e';
+      }
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
